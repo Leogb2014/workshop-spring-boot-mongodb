f56cd6a02f82cddcb14adc02288c22a4feb23a4e
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-02-02 18:38:30 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/leonardogobetti/workshopmongo/domain/Post.java
+++ src/main/java/com/leonardogobetti/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.leonardogobetti.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.leonardogobetti.workshopmongo.dto.AuthorDTO;
+import com.leonardogobetti.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,8 @@ public class Post implements Serializable{
 	private String body;
 	private AuthorDTO author;
 	
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -86,6 +91,17 @@ public class Post implements Serializable{
 	}
 	
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	
+
 
 	@Override
 	public int hashCode() {
@@ -104,6 +120,7 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
 
 
 	
