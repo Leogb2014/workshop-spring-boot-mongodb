6e4d865b7ab0f1f08162e3344b945b3ff499d060
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-02-03 13:08:22 -0300

    Consulta simples com @Query

--- src/main/java/com/leonardogobetti/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leonardogobetti/workshopmongo/repository/PostRepository.java
@@ -3,7 +3,7 @@ package com.leonardogobetti.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.leonardogobetti.workshopmongo.domain.Post;
@@ -12,6 +12,8 @@ import com.leonardogobetti.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title' : { $regex: ?0 , $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
